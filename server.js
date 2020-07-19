8bbef04c6af6efe0e42c2ac0b7d216a8f0ae51c1
Holly Prothe <hollyprothe1@gmail.com>
2020-07-19 16:59:42 -0500

    update server js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/fitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
+// routes
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
